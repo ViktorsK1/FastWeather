0cad1d8b479516922465769594d1922ba6a772ee
Viktor Kulia <vitya.kulya@gmail.com>
2023-04-09 21:57:07 +0300

    init project

---
+++ FastWeather/FastWeather/FastWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+// FastWeatherApp.swift
+// FastWeather
+//
+// Created by Viktor Kulia on 09.04.2023.
+//  
+
+import SwiftUI
+
+@main
+struct FastWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
