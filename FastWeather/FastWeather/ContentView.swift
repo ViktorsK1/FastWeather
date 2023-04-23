02fc274ef622f3b648fa03b7296561d2bb522ad8
Viktor Kulia <vitya.kulya@gmail.com>
2023-04-23 15:04:20 +0300

    added textfield and connected test api

--- FastWeather/FastWeather/ContentView.swift
+++ FastWeather/FastWeather/ContentView.swift
@@ -9,24 +9,33 @@ import SwiftUI
 import CoreLocation
 
 struct ContentView: View {
+    @State private var location: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-        .onAppear {
-            fetchWeatherData()
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Eneter Location", text: $location)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button {
+                        fetchWeatherData(for: location)
+                    } label: {
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .font(.title3)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            .navigationTitle("Mobile Weather")
         }
     }
     
-    func fetchWeatherData() {
+    func fetchWeatherData(for location: String) {
         let apiService = APIService.shared
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
-        CLGeocoder().geocodeAddressString("Paris") { (placemarks, error) in
+        CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
             if let error = error {
                 print(error.localizedDescription)
             }
@@ -51,30 +60,10 @@ struct ContentView: View {
                             print(errorString)
                         }
                     }
+                }
             }
         }
-//        apiService.getJSON(urlString: "https://api.openweathermap.org/data/3.0/onecall?lat=50.429290&lon=30.538060&exclude=current,minutely,hourly,alerts&appid=ed336eb9186b44c07aeee17794ed4690", dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast,APIService.APIError>) in
-//            switch result {
-//            case .success(let forecast):
-//                for day in forecast.daily {
-//                    print(dateFormatter.string(from: day.dt))
-//                    print("  Max: ", day.temp.max)
-//                    print("  Min: ", day.temp.min)
-//                    print("  Humidity: ", day.humidity)
-//                    print("  Description: ", day.weather[0].description)
-//                    print("  Clouds: ", day.clouds)
-//                    print("  pop: ", day.pop)
-//                    print("  IconURL: ", day.weather[0].weatherIconUrl)
-//                }
-//            case .failure(let apiError):
-//                switch apiError {
-//                case .error(let errorString):
-//                    print(errorString)
-//                }
-//            }
-        }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
